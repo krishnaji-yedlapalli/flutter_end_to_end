6f9487f5653763f1713290921a8402162f350ba5
krishnaji-yedlapalli <49545948+krishnaji-yedlapalli@users.noreply.github.com>
2024-06-22 15:22:34 +0530

    551-automation-testing-=-unit-tests-widget-test-integration-test

--- test/widget_testing/school/create_edit_student_test.dart
+++ test/widget_testing/school/create_edit_student_test.dart
@@ -1,16 +1,26 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sample_latest/bloc/school/school_bloc.dart';
+import 'package:sample_latest/environment/environment.dart';
 import 'package:sample_latest/models/school/student_model.dart';
+import 'package:sample_latest/services/repository/school_repository.dart';
 import 'package:sample_latest/ui/schools/create_update_student.dart';
+import 'package:sample_latest/ui/schools/school_details.dart';
+import 'package:sample_latest/utils/device_configurations.dart';
 
 main() {
 
   group('create student widget test', () {
 
-    testWidgets('create new student', (tester) async {
+    setUp((){
+      DeviceConfiguration.initiate();
+    });
 
-      await tester.pumpWidget(const MaterialApp(home: Scaffold(body : CreateStudent('124'))));
+    testWidgets('create new student', (tester) async {
+      await tester.pumpWidget(
+          const MaterialApp(home: Scaffold(body: CreateStudent('124'))));
       await tester.pumpAndSettle();
 
       /// checking validation
@@ -37,11 +47,13 @@ main() {
       expect(find.text('Create'), findsOneWidget);
     });
 
-
     testWidgets('Edit existing student', (tester) async {
-
-      await tester.pumpWidget(MaterialApp(home: Scaffold(body : CreateStudent('123', student:
-      StudentModel('321', '123', 'john', 'texas', 'LKG', 1234567, updatedDate: 432211)))));
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+              body: CreateStudent('123',
+                  student: StudentModel(
+                      '321', '123', 'john', 'texas', 'LKG', 1234567,
+                      updatedDate: 432211)))));
       await tester.pumpAndSettle();
 
       expect(find.text('john'), findsOneWidget);
@@ -58,6 +70,46 @@ main() {
       await tester.pumpAndSettle();
 
       expect(find.text('Update'), findsOneWidget);
+    });
+
+    testWidgets('Testing different Screen resolution', (tester) async {
+
+      const List<Size> screenSizes = [
+         // Size(320, 480), // Small Mobile: e.g., old iPhone SE
+         Size(375, 667), // Regular Mobile: e.g., iPhone 8
+        Size(768, 1024), // Tablet: e.g., iPad
+        Size(1440, 900), // Desktop: Typical resolution
+      ];
+
+      for (var size in screenSizes) {
+        await tester.binding.setSurfaceSize(size);
+        DeviceConfiguration.updateDeviceResolutionAndOrientation(size, Orientation.portrait);
+
+        await tester.pumpWidget(MaterialApp(
+            key: UniqueKey(),
+            home: MediaQuery(
+                data: MediaQueryData(size: size),
+                child:  BlocProvider(
+                    create: (context) => SchoolBloc(SchoolRepository()),
+                    child: SchoolDetails('123', null)))));
+        await tester.pumpAndSettle();
+
+        await tester.tap(find.byType(FloatingActionButton));
+        await tester.pumpAndSettle();
+
+        /// checking validation
+        await tester.tap(find.text('Create'));
+        await tester.pump(const Duration(seconds: 2));
+
+        expect(find.text('Student name is required!!'), findsOneWidget);
+        expect(find.text('Standard is required!!'), findsOneWidget);
+        expect(find.text('Location is required!!'), findsOneWidget);
+
+        var studentTextFieldList = find.byType(TextFormField);
+        await tester.enterText(studentTextFieldList.first, 'Joseph');
+        await tester.enterText(studentTextFieldList.last, 'Texas');
+        await tester.pump();
+      }
     });
   });
 }
