5d357d0c3c42fb6d3709bcadac477759f85d3628
krishnaji-yedlapalli <49545948+krishnaji-yedlapalli@users.noreply.github.com>
2025-09-25 22:03:26 +0530

    1112-functional-changes-for-daily-tracker

--- scripts/pre_commit_hook.sh
+++ scripts/pre_commit_hook.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Find the Git repository root dynamically
+GIT_ROOT=$(git rev-parse --show-toplevel)
+if [ -z "$GIT_ROOT" ]; then
+  echo "Error: Not a Git repository or git not found."
+  exit 1
+fi
+
+# Change to the Git root directory
+cd "$GIT_ROOT" || exit 1
+
 echo "Running pre-commit checks for daily_tracker..."
 
 # Run Dart analyzer
