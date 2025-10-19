3c246ad5c9dfd82eb0f977f67aa16f964dac5ca6
krishnaji-yedlapalli <49545948+krishnaji-yedlapalli@users.noreply.github.com>
2025-10-19 22:08:46 +0530

    feat: Add custom commit message validation hook

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-// This file is for configuring commitlint, a tool that lints our commit messages.
-// We use it to enforce the Conventional Commits standard, which is important for
-// our automated release and versioning workflow.
-// For more information, see: https://commitlint.js.org/
-
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-};
