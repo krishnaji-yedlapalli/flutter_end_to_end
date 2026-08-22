8410dbdce2b6d3c2faa005e8e0c12acd621ed963
krishnaji-yedlapalli <49545948+krishnaji-yedlapalli@users.noreply.github.com>
2026-08-22 06:47:56 +0530

    feat: disabled app update feature

--- lib/features/dashboard/home_screen.dart
+++ lib/features/dashboard/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:app_core/core/firebase/config/remote_config_keys.dart';
 import 'package:app_core/core/firebase/config/remote_config_scope.dart';
 import 'package:app_core/core/utils/connectivity_handler.dart';
 import 'package:app_core/core/utils/enums_type_def.dart';
-import 'package:app_update_feature/core/app_update_startup_check.dart';
+// import 'package:app_update_feature/core/app_update_startup_check.dart';
 import 'package:feature_discovery_module/home_feature_discovery.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -69,8 +69,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> showFeatureDiscovery() async {
-    final allow = await AppUpdateStartupCheck.performCheck(context);
-    if (allow) HomeScreenFeatureDiscovery().startFeatureDiscovery(context);
+    // final allow = await AppUpdateStartupCheck.performCheck(context);
+    // if (allow)
+    HomeScreenFeatureDiscovery().startFeatureDiscovery(context);
   }
 
   @override
